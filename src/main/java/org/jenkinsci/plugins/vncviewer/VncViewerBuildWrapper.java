3892dbbfdee86c1695591ff1ba092df6f52ea5ea
bento silveira <bento.silveira@entelgy.com>
2019-04-11 14:25:44 -0300

    Changed so the URL points to the original server, instead of proxying all for the sake of Docker

--- src/main/java/org/jenkinsci/plugins/vncviewer/VncViewerBuildWrapper.java
+++ src/main/java/org/jenkinsci/plugins/vncviewer/VncViewerBuildWrapper.java
@@ -94,6 +94,7 @@ public class VncViewerBuildWrapper extends BuildWrapper {
         {
             vncServReplaced = vncServReplaced.replace(":", ":59");
         }
+        String vncServPort = vncServReplaced.split(":")[1];
 
         try {
             untar(VncViewerBuildWrapper.class.getResourceAsStream("/novnc.tar"),System.getProperty("java.io.tmpdir"));
@@ -126,7 +127,8 @@ public class VncViewerBuildWrapper extends BuildWrapper {
         }
 
         String hostAddr = determineJenkinsHostAddress(listener);
-        String url = "http://" + hostAddr + ":" + lp + "/vnc_auto.html?host=" + hostAddr + "&port=" + lp;
+        // this proxy thing don't work on docker, so point noVnc to the original host
+        String url = "http://" + hostAddr + ":" + lp + "/vnc_auto.html?host=" + vncServReplaced + "&port=" + vncServPort;
         String txt = "Start vnc viewer for " + vncServReplaced;
         listener.getLogger().print('\n');
         listener.annotate(new ConsoleNoteButton(txt,url));
